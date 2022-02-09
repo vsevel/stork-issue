aa1f94408f3b78941d528019d4f41ce00f6667f3
Vincent Sevel <vincent@sevel.eu>
2022-02-09 12:11:04 +0100

    service discovery resilience

--- src/main/java/org/acme/MyServiceDiscovery.java
+++ src/main/java/org/acme/MyServiceDiscovery.java
@@ -1,20 +1,31 @@
 package org.acme;
 
 import io.smallrye.mutiny.Uni;
-import io.smallrye.stork.api.ServiceDiscovery;
 import io.smallrye.stork.api.ServiceInstance;
+import io.smallrye.stork.impl.CachingServiceDiscovery;
 import io.smallrye.stork.impl.DefaultServiceInstance;
 import io.smallrye.stork.utils.ServiceInstanceIds;
+import org.jboss.logging.Logger;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MyServiceDiscovery implements ServiceDiscovery {
+public class MyServiceDiscovery extends CachingServiceDiscovery {
+
+    Logger log = Logger.getLogger(MyServiceDiscovery.class);
+
+    public MyServiceDiscovery() {
+        super("10S");
+    }
 
     @Override
-    public Uni<List<ServiceInstance>> getServiceInstances() {
+    public Uni<List<ServiceInstance>> fetchNewServiceInstances(List<ServiceInstance> previousInstances) {
+        log.info("fetching new instances");
+        if (System.currentTimeMillis() % 2 == 0) {
+            throw new RuntimeException("oops registry not available");
+        }
         return Uni.createFrom().item(Arrays.asList(
-                new DefaultServiceInstance(ServiceInstanceIds.next(), "localhost", 8081, false),
+                new DefaultServiceInstance(ServiceInstanceIds.next(), "localhost", 8080, false),
                 new DefaultServiceInstance(ServiceInstanceIds.next(), "localhost", 8080, false)
         ));
     }
